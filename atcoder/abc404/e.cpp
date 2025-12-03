7b4f7b1cd8c18861e2ae6e43c957cfff78fcd590
harsha314 <harshavardhan32952@gmail.com>
2025-12-03 16:40:01 +0530

    abc404/e

--- atcoder/abc404/e.cpp
+++ atcoder/abc404/e.cpp
@@ -21,21 +21,20 @@ void solve() {
   vector<int> a(n), c(n);
   for (int i = 1; i < n; ++i) cin >> c[i];
   for (int i = 1; i < n; ++i) cin >> a[i];
-  vector<int> dp(n, n);
-  dp[0] = 0;
+
+  int ans = 0, pre = 0;
   for (int i = 1; i < n; ++i) {
-    // if (a[i] > 0) {
-    for (int j = i - 1; j >= i - c[i] && j >= 0; --j) {
-      if (a[j] > 0 || j == 0) dp[i] = min(dp[i], 1 + dp[j]);
+    if (a[i] > 0) {
+      int l = i, r = i;
+      while (pre < l) {
+        int nl = l;
+        for (int j = l; j <= r; ++j) nl = min(nl, j - c[j]);
+        r = l;
+        l = nl;
+        ++ans;
+      }
+      pre = i;
     }
-    // } else
-    // dp[i] = dp[i - 1];
   }
-  int ans = 0;
-  for (int i = 0; i < n; ++i) {
-    if (a[i] > 0) ans = max(ans, dp[i]);
-  }
-  for (int& x : dp) cout << x << " ";
-  cout << "\n";
   cout << ans << "\n";
 }
